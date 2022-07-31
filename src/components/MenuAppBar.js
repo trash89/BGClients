08c925dc233550e4d04c1924d0d30a684833512f
m r <trash89@laposte.net>
2022-07-31 21:42:13 +0200

    upd

--- src/components/MenuAppBar.js
+++ src/components/MenuAppBar.js
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import { supabase } from "../supabaseClient";
-import { removeUserFromLocalStorage } from "../utils/localStorage";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { logoutUser } from "../features/user/userSlice";
-import { links, userLinks } from "../utils/links";
+import { links } from "../utils/links";
 import Logo from "./Logo";
 
 const MenuAppBar = () => {
   const { user } = useSelector((store) => store.user);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   return (
     <nav className="navbar navbar-expand-sm bg-primary navbar-dark sticky-top">
       <div className="container-fluid">
@@ -24,38 +19,40 @@ const MenuAppBar = () => {
           <ul className="navbar-nav">
             {links.map((page) => {
               return (
-                <li key={page.id} className="nav-item">
+                <li key={page.id} className="nav-item text-capitalize">
                   <a className="nav-link" href={page.path}>
                     {page.text}
                   </a>
                 </li>
               );
             })}
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                {user.Username}
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="/">
-                    profiles
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="dropdown-item"
-                    href="/"
-                    onClick={async () => {
-                      await supabase.auth.signOut();
-                      removeUserFromLocalStorage();
-                    }}
-                  >
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </li>
           </ul>
+        </div>
+        <div className="d-flex">
+          <div className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle bg-primary text-white" href="/" role="button" data-bs-toggle="dropdown">
+              {user.Username}
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item text-capitalize" href="/profiles">
+                  profiles
+                </a>
+              </li>
+              <li>
+                <a
+                  className="dropdown-item text-capitalize"
+                  href="/"
+                  onClick={async () => {
+                    await supabase.auth.signOut();
+                    logoutUser();
+                  }}
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
