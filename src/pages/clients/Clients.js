9d448818da06dd536bdf7fba6c2d2e30497120fc
m r <trash89@laposte.net>
2022-08-03 22:44:58 +0200

    upd

--- src/pages/clients/Clients.js
+++ src/pages/clients/Clients.js
@@ -5,11 +5,21 @@ import { useSelector, useDispatch } from "react-redux";
 import { useIsMounted } from "../../hooks";
 import { dateFormat } from "../../utils/constants";
 import { Progress } from "../../components";
+import { supabase } from "../../supabaseClient";
 
 const Clients = () => {
   const isMounted = useIsMounted();
   const { user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getClients = async () => {
+      const { data, error } = await supabase.from("clients").select();
+      console.log(data);
+    };
+
+    getClients();
+  }, []);
 
   if (!isMounted) return <></>;
   //if (loading) return <Progress />;
