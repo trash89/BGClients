96cc13de027fedd95bd305c64dfa79f0c1d3dd89
m r <trash89@laposte.net>
2022-08-22 17:49:03 +0200

    upd

--- src/components/ClientViewComponent.js
+++ src/components/ClientViewComponent.js
@@ -1,5 +1,43 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { axiosInstance } from "../axiosInstance";
+import { useSelector, useDispatch } from "react-redux";
+import { useIsMounted } from "../hooks";
+import { Progress } from "../components";
+import { setInput, setIsLoading, clearIsLoading, setError, clearError, setEdit, clearValues } from "../features/client/clientSlice";
+
 const ClientViewComponent = ({ user }) => {
-  return <div>Client view component, user={JSON.stringify(user, null, 2)}</div>;
+  const isMounted = useIsMounted();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const params = useParams();
+
+  const { input, isLoading, isError, errorText } = useSelector((store) => store.client);
+
+  useEffect(() => {
+    const getData = async () => {
+      dispatch(setIsLoading());
+      try {
+        const resp = await axiosInstance.post("/clientview", { id: user.id, email: user.email });
+        console.log(resp.data);
+      } catch (error) {
+        console.log(error);
+        dispatch(setError(error.response.data.error.message));
+      } finally {
+        dispatch(clearIsLoading());
+      }
+    };
+    getData();
+  }, []);
+
+  if (!isMounted) return <></>;
+  if (isLoading) return <Progress />;
+
+  return (
+    <div>
+      Client view component, user={user.email} {user.id}
+    </div>
+  );
 };
 
 export default ClientViewComponent;
